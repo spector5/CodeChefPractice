f4e42b729b3b5739fe65e032a8b2c1c96bd67178
tBostic <trb004@eagles.bridgewater.edu>
2015-10-14 15:00:36 -0400

    Here are  my own submissions. The input test and obediant servant were both very easy, just to make sure everything was working properly. The others are a bit harder.

---
+++ CodeChefPractice/src/MatrixSteps.java
@@ -0,0 +1,72 @@
+/***
+ * @url https://www.codechef.com/problems/MSTEP
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+class MatrixSteps {
+	
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter out=new PrintWriter(System.out);
+	
+	public static void main(String[] args) throws IOException
+	{
+		int numTestCases = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < numTestCases; i++)
+		{
+			int n = Integer.parseInt(br.readLine());
+			if(n == 1)
+			{
+				br.readLine();
+				out.println("0");
+				continue;
+			}
+			OrderedPair[] coordinates = new OrderedPair[(n*n) + 1];
+			
+			for(int x = 1; x <= n; x++)
+			{
+				int num = 0;
+				String[] s = br.readLine().split(" ");
+				for(int k = 1; k <= n; k++)
+				{
+					num = Integer.parseInt(s[k - 1]);
+					coordinates[num] = new OrderedPair(x,k);
+				}
+			}
+			
+			int steps = 0;
+			int prevRow;
+			int prevCol;
+			
+			prevRow = coordinates[1].row;
+			prevCol = coordinates[1].col;
+			
+			for(int y = 2; y <= n * n; y++)
+			{
+				steps += Math.abs(prevRow - coordinates[y].row) + Math.abs(prevCol - coordinates[y].col);
+				prevRow = coordinates[y].row;
+				prevCol = coordinates[y].col;
+			}
+			out.println(steps);
+			
+		}
+		out.close();
+	}
+
+}
+
+class OrderedPair
+{
+	int row;
+	int col;
+	
+	public OrderedPair(int row, int col)
+	{
+		this.row = row;
+		this.col = col;
+	}
+}
---
+++ CodeChefPractice/src/NonDecreasingArrays.java
@@ -0,0 +1,45 @@
+/**
+ * @url https://www.codechef.com/OCT15/problems/SUBINC
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class NonDecreasingArrays {
+	
+	public static void main(String args[]) throws NumberFormatException, IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testCases = Integer.parseInt(br.readLine());
+		int arrayLength = 0;
+		
+		for(int i = 0; i < testCases; i++)
+		{
+			arrayLength = Integer.parseInt(br.readLine());
+			int[] array = new int[arrayLength];
+			String line = br.readLine();
+			String[] elements = line.split(" ");
+			int x = 0;
+			for(String element : elements)
+			{
+				array[x++] = Integer.parseInt(element);
+			}
+			
+			int counter = 0; 
+			for(int start = 0; start < array.length; start++)
+			{
+				int t = 1;
+				while((start + t) < array.length && array[start + (t-1)] <= array[start + t])
+				{
+					t++;
+				}
+				
+				counter += (t*(t+1))/2;
+				start += t - 1;
+			}
+			
+			System.out.println(counter);
+		}
+	}
+
+}
