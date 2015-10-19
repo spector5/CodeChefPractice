37cee725299dcb87098f4af699083bb46f33a29f
tBostic <trb004@eagles.bridgewater.edu>
2015-10-19 09:22:12 -0400

    Final version of the prime generator that finds all of the primes between 2 given numbers. Apparently, printing out each time a solution was found was the fastest in this case. The isPrime function in this program is fantastic. I beat all other java solutions that I could find on CodeChef by at least 3 tenths of a second.

--- src/Trevor/PrimeGenerator.java
+++ src/Trevor/PrimeGenerator.java
@@ -4,33 +4,39 @@ package Trevor;
  */
 import java.io.*;
 import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class PrimeGenerator {
 
 	public static void main(String[] args) throws NumberFormatException, IOException 
 	{
+		//long startTime = System.currentTimeMillis();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BigInteger min;
-		BigInteger max;
+		//BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 		int t = Integer.parseInt(br.readLine());
 		while(t-- > 0)
 		{
-			String[] line = br.readLine().split(" ");
-			min = new BigInteger(line[0]);
-			max = new BigInteger(line[1]);
-			BigInteger temp;
-			double num;
-			while(min.compareTo(max) < 0)
+			String[] numbers = br.readLine().split(" ");
+			int min = Integer.parseInt(numbers[0]);
+			int max = Integer.parseInt(numbers[1]);
+			/*int min = 1000;
+			int max = 101000;*/
+			//String line = "";
+			for(int i = min; i <= max; i++)
 			{
-				num = min.doubleValue();
-				//if(isPrime(num))
-				System.out.println((int) num);
-				
-				min = min.nextProbablePrime();
+				if(isPrime(i))
+					System.out.println(i);
+
 			}
+			//System.out.println(line);
 		}
+		
+		/*long endTime = System.currentTimeMillis();
+		SimpleDateFormat sdf = new SimpleDateFormat("m:s:S");
+		System.out.println("\nTotal time [min:sec:milli]: " + sdf.format(new Date(endTime - startTime)));*/
 	}
 	
-	public static boolean isPrime(double num)
+	public static boolean isPrime(int num)
 	{
 		if(num <= 1)
 			return false;
