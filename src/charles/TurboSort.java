910c9c0542945314472303ae0c35ee033cd9db56
tBostic <trb004@eagles.bridgewater.edu>
2015-10-22 22:20:24 -0400

    Turbo Sort Fixed
    Crazy, but changing it to a bufferedwriter helped its runtime so much
    that it took the runtime from over 5 seconds to about a half a second.

--- src/charles/TurboSort.java
+++ src/charles/TurboSort.java
@@ -5,8 +5,10 @@ package charles;
  * NOTE: this solution is too slow
  */
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.Arrays;
 
@@ -14,7 +16,8 @@ class TurboSort {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// setup
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter out = new PrintWriter(System.out, true);
+		//PrintWriter out = new PrintWriter(System.out, true);
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		// input
 		int t = Integer.parseInt(in.readLine()); // num items
 		int[] nums = new int[t]; // array of nums
@@ -28,7 +31,8 @@ class TurboSort {
 		Arrays.sort(nums); // sorts array
 		// output
 		for (int i = 0; i < t; i++) {
-			System.out.println(nums[i]);
+			out.write((nums[i] + "\n"));
 		}
+		out.flush();
 	}
 }
