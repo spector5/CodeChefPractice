b84c489e555204a2abaf792d47b73c1f2c56bcbc
5donuts <5donuts@users.noreply.github.com>
2015-10-28 20:30:21 -0400

    added stuff for cooling pies problem

---
+++ src/charles/CoolingPies.java
@@ -0,0 +1,31 @@
+package charles;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * @url https://www.codechef.com/problems/COOLING
+ * @author 5donuts
+ *
+ */
+class CoolingPies {
+	public static void main(String[] args) throws IOException {
+		// setup
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		// input
+		int t = Integer.parseInt(in.readLine()); // test cases
+		int n; // number if pies & racks for each test case
+		// actions
+		for (int i = 0; i < t; i++) {
+			
+		}
+		// output
+		out.flush();
+		// cleanup
+		out.close();
+	}
+}
