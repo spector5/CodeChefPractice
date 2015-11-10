382e3456072dffec31c41368708e6080b65ffc09
5donuts <5donuts@users.noreply.github.com>
2015-11-09 20:35:50 -0500

    skeleton for Cooling Pies problem

--- src/charles/CoolingPies.java
+++ src/charles/CoolingPies.java
@@ -19,10 +19,28 @@ class CoolingPies {
 		// input
 		int t = Integer.parseInt(in.readLine()); // test cases
 		int n; // number if pies & racks for each test case
+		int[] pies; // pies array
+		int[] racks; // racks array
 		// actions
+		/*
+		 * gets pie and rack weights
+		 */
 		for (int i = 0; i < t; i++) {
-			
+			n = Integer.parseInt(in.readLine());
+			pies = new int[n];
+			racks = new int[n];
+			// reads pie weights
+			for (int j = 0; j < n; j++) {
+				pies[j] = Integer.parseInt(in.readLine());
+			}
+			// reads rack weights
+			for (int j = 0; j < n; j++) {
+				racks[j] = Integer.parseInt(in.readLine());
+			}
 		}
+		/*
+		 * determine how many pies can fit on each rack
+		 */
 		// output
 		out.flush();
 		// cleanup
