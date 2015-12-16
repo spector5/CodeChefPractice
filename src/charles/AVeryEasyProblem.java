4745ccc20b30341507b0e13f800e564e76708714
5donuts <5donuts@users.noreply.github.com>
2015-12-15 21:58:01 -0500

    added 'a very easy problem'

---
+++ src/charles/AVeryEasyProblem.java
@@ -0,0 +1,24 @@
+package charles;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * link: https://www.codechef.com/problems/EASYPROB
+ *
+ */
+public class AVeryEasyProblem {
+	public static void main(String[] args) {
+		// setup
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		// actions
+		// output
+		out.flush();
+		// cleanup
+		in.close();
+		out.close();
+	}
+}
